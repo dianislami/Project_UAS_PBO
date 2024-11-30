c678bf73cf94d1b033c8ed7b06eac4bbbad484e8
firahmaulida <163720090+firahmaulida@users.noreply.github.com>
2024-11-30 21:52:43 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Admin extends Akun {
-    private List<Product> products;
+    private List<Barang> products; // Menggunakan Barang sebagai tipe data untuk produk
 
     public Admin(String id, String username, String password) {
         super(id, username, password);
@@ -14,15 +14,14 @@ public class Admin extends Akun {
         return this.username.equals(username) && this.password.equals(password);
     }
 
-    public void addProduct(Product product) {
-        products.add(product);
-        System.out.println("Barang berhasil ditambahkan: " + product);
+    public void addBarang(Barang barang) {
+        products.add(barang);
     }
 
-    public void editProduct(String productId, Product updatedProduct) {
+    public void editBarang(String productId, Barang updatedBarang) {
         for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getId().equals(productId)) {
-                products.set(i, updatedProduct);
+            if (products.get(i).getIdBarang().equals(productId)) {
+                products.set(i, updatedBarang);
                 System.out.println("Barang berhasil diubah.");
                 return;
             }
@@ -30,27 +29,27 @@ public class Admin extends Akun {
         System.out.println("Barang dengan ID tersebut tidak ditemukan.");
     }
 
-    public void removeProduct(String productId) {
-        products.removeIf(product -> product.getId().equals(productId));
+    public void removeBarang(String productId) {
+        products.removeIf(product -> product.getIdBarang().equals(productId));
         System.out.println("Barang berhasil dihapus.");
     }
 
-    public void viewProducts() {
+    public void viewBarang() {
         if (products.isEmpty()) {
             System.out.println("Tidak ada barang tersedia.");
         } else {
             System.out.println("+----+---------------+---------+-------+");
             System.out.println("| ID | Nama Barang   | Harga   | Stok  |");
             System.out.println("+----+---------------+---------+-------+");
-            for (Product product : products) {
+            for (Barang product : products) {
                 System.out.printf("| %-2s | %-13s | %-7.0f | %-5d |\n",
-                        product.getId(), product.getName(), product.getPrice(), product.getStock());
+                        product.getIdBarang(), product.getNamaBarang(), product.getHargaBarang(), product.getStokBarang());
             }
             System.out.println("+----+---------------+---------+-------+");
         }
     }
 
-    public List<Product> getProducts() {
+    public List<Barang> getProducts() {
         return products;
     }
 }
