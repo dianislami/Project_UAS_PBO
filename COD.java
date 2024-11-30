0fb7ac17e7de0996900d06a7ad1223a91d2399ac
dian islami <163720568+dianislami@users.noreply.github.com>
2024-11-30 20:04:24 +0700

    Update COD.java

--- COD.java
+++ COD.java
@@ -1,10 +1,16 @@
 public class COD extends Pembayaran {
-    public COD() {
-        super("Cash on Delivery");
+    public COD(String id) {
+        super(id);
     }
 
     @Override
-    public void prosesPembayaran(double jumlah) {
-        System.out.println("Pembayaran sebesar " + jumlah + " akan dilakukan secara tunai saat barang diterima (COD).");
+    public String getJenisPembayaran() {
+        return "COD";
+    }
+
+    @Override
+    public void prosesPembayaran() {
+        System.out.println("Pembayaran dilakukan melalui COD dengan ID: " + id);
+        System.out.println("Silakan serahkan pembayaran ke kurir saat barang diterima.");
     }
 }
