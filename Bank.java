6fc62f8ca04ae7bc58e797057dcd6de699d3690b
firahmaulida <163720090+firahmaulida@users.noreply.github.com>
2024-11-30 21:54:52 +0700

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank extends Pembayaran {
+    private static final Map<String, String[]> daftarBank = new HashMap<>();
+
+    static {
+        // Menambahkan daftar bank, nomor rekening, dan nama pemilik rekening
+        daftarBank.put("Bank Aceh", new String[]{"1234567890", "Pinkypie"});
+        daftarBank.put("BSI", new String[]{"2345678901", "Pinkypie"});
+        daftarBank.put("BCA", new String[]{"3456789012", "Pinkypie"});
+        daftarBank.put("Mandiri", new String[]{"4567890123", "Pinkypie"});
+    }
+
+    public Bank(String id) {
+        super(id);
+    }
+
+    @Override
+    public String getJenisPembayaran() {
+        return "Bank";
+    }
+
+    @Override
+    public void prosesPembayaran() {
+        System.out.println("Pembayaran melalui transfer Bank dengan ID: " + id);
+        tampilkanDaftarBank(); // Menampilkan daftar bank saat pembayaran dilakukan
+    }
+
+    private void tampilkanDaftarBank() {
+        System.out.println("\nDaftar Bank yang Tersedia:");
+        for (String bank : daftarBank.keySet()) {
+            String[] info = daftarBank.get(bank);
+            System.out.println("- " + bank + ":");
+            System.out.println("  Nomor Rekening: " + info[0]);
+            System.out.println("  Nama Pemilik  : " + info[1]);
+        }
+        System.out.println("Silakan transfer ke salah satu rekening di atas.\n");
+    }
+}
