48b31ad96b3e9cd64cdfc8a75eb2460638f7b633
dian islami <163720568+dianislami@users.noreply.github.com>
2024-11-30 20:03:04 +0700

    Update Akun.java

--- Akun.java
+++ Akun.java
@@ -1,7 +1,7 @@
 public abstract class Akun {
-    private String id;         
-    private String username;   
-    private String password;   
+    protected String id;
+    protected String username;
+    protected String password;
 
     public Akun(String id, String username, String password) {
         this.id = id;
@@ -9,41 +9,18 @@ public abstract class Akun {
         this.password = password;
     }
 
-    // Getter dan Setter
+    // Getter methods
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    // Metode untuk validasi login
-    public boolean login(String un, String pw) {
-        return this.username.equals(un) && this.password.equals(pw);
-    }
-
-    // Method untuk logout
-    public void logout() {
-        System.out.println("Akun dengan ID " + id + " berhasil logout.");
-    }
-
-    // Metode abstrak (harus diimplementasikan oleh subclass)
-    public abstract void displayRole();
+    public abstract boolean login(String username, String password);
 }
