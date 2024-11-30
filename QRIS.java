331682f2d9018cc032767e0258889dbb00da1ecb
dian islami <163720568+dianislami@users.noreply.github.com>
2024-11-30 20:11:09 +0700

    Update QRIS.java

--- QRIS.java
+++ QRIS.java
@@ -1,10 +1,24 @@
 public class QRIS extends Pembayaran {
-    public QRIS() {
-        super("QRIS");
+    public QRIS(String id) {
+        super(id);
     }
 
     @Override
-    public void prosesPembayaran(double jumlah) {
-        System.out.println("Pembayaran sebesar " + jumlah + " menggunakan QRIS berhasil diproses.");
+    public String getJenisPembayaran() {
+        return "QRIS";
+    }
+
+    @Override
+    public void prosesPembayaran() {
+        tampilkanQRCode(); // Tampilkan QR Code saat proses pembayaran dimulai
+        System.out.println("Pembayaran berhasil melalui QRIS dengan ID: " + id);
+    }
+
+    private void tampilkanQRCode() {
+        System.out.println("\n[QR Code]");
+        System.out.println("|######################|");
+        System.out.println("|      SCAN ME!       |");
+        System.out.println("|######################|");
+        System.out.println("Gunakan aplikasi pembayaran untuk memindai QR Code.\n");
     }
 }
