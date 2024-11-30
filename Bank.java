dcef0e21865896ad6c6741ccec90db68ece35ead
dian islami <163720568+dianislami@users.noreply.github.com>
2024-11-30 20:03:34 +0700

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,17 +1,40 @@
-public class Bank extends Pembayaran {
-    private String namaBank;
+import java.util.HashMap;
+import java.util.Map;
 
-    public Bank(String namaBank) {
-        super("Bank Transfer");
-        this.namaBank = namaBank;
+public class Bank extends Pembayaran {
+    private static final Map<String, String[]> daftarBank = new HashMap<>();
+
+    static {
+        // Menambahkan daftar bank, nomor rekening, dan nama pemilik rekening
+        daftarBank.put("Bank Aceh", new String[]{"1234567890", "Pinkypie"});
+        daftarBank.put("BSI", new String[]{"2345678901", "Pinkypie"});
+        daftarBank.put("BCA", new String[]{"3456789012", "Pinkypie"});
+        daftarBank.put("Mandiri", new String[]{"4567890123", "Pinkypie"});
     }
 
-    public String getNamaBank() {
-        return namaBank;
+    public Bank(String id) {
+        super(id);
     }
 
     @Override
-    public void prosesPembayaran(double jumlah) {
-        System.out.println("Pembayaran sebesar " + jumlah + " melalui bank " + namaBank + " berhasil diproses.");
+    public String getJenisPembayaran() {
+        return "Bank";
+    }
+
+    @Override
+    public void prosesPembayaran() {
+        System.out.println("Pembayaran melalui transfer Bank dengan ID: " + id);
+        tampilkanDaftarBank(); // Menampilkan daftar bank saat pembayaran dilakukan
+    }
+
+    private void tampilkanDaftarBank() {
+        System.out.println("\nDaftar Bank yang Tersedia:");
+        for (String bank : daftarBank.keySet()) {
+            String[] info = daftarBank.get(bank);
+            System.out.println("- " + bank + ":");
+            System.out.println("  Nomor Rekening: " + info[0]);
+            System.out.println("  Nama Pemilik  : " + info[1]);
+        }
+        System.out.println("Silakan transfer ke salah satu rekening di atas.\n");
     }
 }
