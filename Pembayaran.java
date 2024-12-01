ec4e071cb8bd2357b979c033c78c94243bc1b262
dian islami <163720568+dianislami@users.noreply.github.com>
2024-12-01 11:23:52 +0700

    Update Pembayaran.java

--- Pembayaran.java
+++ Pembayaran.java
@@ -1,14 +1,19 @@
 public abstract class Pembayaran {
     protected String id; // ID pembayaran
 
+    // Constructor untuk membuat objek pembayaran dengan ID tertentu
     public Pembayaran(String id) {
         this.id = id;
     }
 
+    // Method untuk mendapatkan ID pembayaran
     public String getId() {
         return id;
     }
 
+    // Method abstrak untuk mendapatkan jenis pembayaran (contoh: Kartu Kredit, Transfer Bank)
     public abstract String getJenisPembayaran();
+
+    // Method abstrak untuk memproses pembayaran sesuai jenis pembayaran
     public abstract void prosesPembayaran();
 }
