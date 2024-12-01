dd81e65aedcd642e91d11b7dd03917bfaa287370
dian islami <163720568+dianislami@users.noreply.github.com>
2024-12-01 11:24:50 +0700

    Update QRIS.java

--- QRIS.java
+++ QRIS.java
@@ -1,6 +1,6 @@
 public class QRIS extends Pembayaran {
     public QRIS(String id) {
-        super(id);
+        super(id); // Memanggil constructor dari kelas induk (Pembayaran)
     }
 
     @Override
@@ -9,11 +9,13 @@ public class QRIS extends Pembayaran {
     }
 
     @Override
+    // Method untuk memproses pembayaran menggunakan QRIS
     public void prosesPembayaran() {
         tampilkanQRCode(); // Tampilkan QR Code saat proses pembayaran dimulai
         System.out.println("Pembayaran berhasil melalui QRIS dengan ID: " + id);
     }
 
+    // Method untuk menampilkan QR Code sebagai simulasi pembayaran
     private void tampilkanQRCode() {
         System.out.println("\n[QR Code]");
         System.out.println("|######################|");
