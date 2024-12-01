2e1f7bbe2cc377d12d6e7bf7d021cf6dafcf5724
dian islami <163720568+dianislami@users.noreply.github.com>
2024-12-01 11:06:26 +0700

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -17,12 +17,12 @@ public class Bank extends Pembayaran {
     }
 
     @Override
-    public String getJenisPembayaran() {
+    public String getJenisPembayaran() { // Mengembalikan jenis pembayaran yaitu "Bank"
         return "Bank";
     }
 
     @Override
-    public void prosesPembayaran() {
+    public void prosesPembayaran() { // Menjalankan proses pembayaran melalui bank
         System.out.println("Pembayaran melalui transfer Bank dengan ID: " + id);
         tampilkanDaftarBank(); // Menampilkan daftar bank saat pembayaran dilakukan
     }
@@ -30,11 +30,13 @@ public class Bank extends Pembayaran {
     private void tampilkanDaftarBank() {
         System.out.println("\nDaftar Bank yang Tersedia:");
         for (String bank : daftarBank.keySet()) {
+            // Mengambil informasi bank, termasuk nomor rekening dan nama pemilik
             String[] info = daftarBank.get(bank);
             System.out.println("- " + bank + ":");
             System.out.println("  Nomor Rekening: " + info[0]);
             System.out.println("  Nama Pemilik  : " + info[1]);
         }
+        // Memberikan instruksi untuk transfer ke salah satu rekening
         System.out.println("Silakan transfer ke salah satu rekening di atas.\n");
     }
 }
